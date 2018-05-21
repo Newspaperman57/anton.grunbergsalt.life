06392e51bcb0efc960b415a11870cc90acc8915e
Phihili <you@example.com>
2018-05-21 15:48:34 +0200

    Change politics

--- addSalt.php
+++ addSalt.php
@@ -1,7 +1,7 @@
 <?php 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	echo "Added \"" . $_POST['text'] . "\" to GrunbergSalt.life!";
-	$_POST['text'] .= "\n" . file_get_contents('grunbergsalt.csv');
-	file_put_contents('grunbergsalt.csv', $_POST['text']);
+	$fileContent = $_POST['text'] . "\n" . file_get_contents('grunbergsalt.csv');
+	file_put_contents('grunbergsalt.csv', $fileContent);
 }
 ?>
