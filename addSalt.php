97a0e84965c557f2fb49e589e77f8bdeaa5947bb
Phihili <you@example.com>
2018-05-21 15:42:09 +0200

    Update to addSalt.php

--- addSalt.php
+++ addSalt.php
@@ -1,5 +1,5 @@
 <?php 
-
-echo "Added " . $_POST['text'] . " to grunbergsalt.info!";
-
+$_POST['text'] .= "\n" . file_get_contents('grunbergSalt.csv');
+file_put_contents('grunbergSalt.csv', $_POST['text']);
+echo "Added \"" . $_POST['text'] . "\" to grunbergsalt.life!";
 ?>
