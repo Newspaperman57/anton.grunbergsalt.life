7eb3c6f81efff06956a0a399a69d0f5a4c303428
Phihili <you@example.com>
2018-05-21 15:44:34 +0200

    Final update to addSalt

--- addSalt.php
+++ addSalt.php
@@ -1,5 +1,7 @@
 <?php 
-echo "Added \"" . $_POST['text'] . "\" to grunbergsalt.life!";
-$_POST['text'] .= "\n" . file_get_contents('grunbergsalt.csv');
-file_put_contents('grunbergsalt.csv', $_POST['text']);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	echo "Added \"" . $_POST['text'] . "\" to GrunbergSalt.life!";
+	$_POST['text'] .= "\n" . file_get_contents('grunbergsalt.csv');
+	file_put_contents('grunbergsalt.csv', $_POST['text']);
+}
 ?>
