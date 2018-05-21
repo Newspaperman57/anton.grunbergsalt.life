85dd5872d88ee9d25893668e206130f4949f2547
Phihili <you@example.com>
2018-05-21 16:07:20 +0200

    Translates message to danish

--- addSalt.php
+++ addSalt.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	if($_POST['text'] == "proclaim") {
 		$data['text'] = "En slackbot selvfølgelig! Skriv /salt efterfulgt af Grünbergs salt og det ender direkte på https://GrunbergSalt.life!";
 	} else {
-		$data['text'] = "Added \"" . $_POST['text'] . "\" to https://GrunbergSalt.life!\"";
+		$data['text'] = "\"" . $_POST['text'] . "\" er nu tilføjet til https://GrunbergSalt.life!\"";
 		$fileContent = $_POST['text'] . "\n" . file_get_contents('grunbergsalt.csv');
 		file_put_contents('grunbergsalt.csv', $fileContent);
 	}
