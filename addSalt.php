99425ec50d9ea5b98b1068e98c9a9efd2c7e4ce8
Phihili <you@example.com>
2018-05-21 15:43:04 +0200

    Update to addSalt.php

--- addSalt.php
+++ addSalt.php
@@ -1,5 +1,5 @@
 <?php 
-$_POST['text'] .= "\n" . file_get_contents('grunbergSalt.csv');
-file_put_contents('grunbergSalt.csv', $_POST['text']);
 echo "Added \"" . $_POST['text'] . "\" to grunbergsalt.life!";
+$_POST['text'] .= "\n" . file_get_contents('grunbergsalt.csv');
+file_put_contents('grunbergsalt.csv', $_POST['text']);
 ?>
