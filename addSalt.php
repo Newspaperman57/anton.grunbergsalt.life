fa85dd714262cd7d8ec89c85113abb32293dc9a0
Phihili <you@example.com>
2018-05-21 15:54:37 +0200

    Added proclaim command

--- addSalt.php
+++ addSalt.php
@@ -1,7 +1,11 @@
 <?php 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	echo "Added \"" . $_POST['text'] . "\" to GrunbergSalt.life!";
-	$fileContent = $_POST['text'] . "\n" . file_get_contents('grunbergsalt.csv');
-	file_put_contents('grunbergsalt.csv', $fileContent);
+	if($_POST['text'] == "proclaim") {
+		echo "En slackbot selvfølgelig! Skriv /salt efterfulgt af Grünbergs salt og det ender direkte på GrunbergSalt.life!";
+	} else {
+		echo "Added \"" . $_POST['text'] . "\" to GrunbergSalt.life!";
+		$fileContent = $_POST['text'] . "\n" . file_get_contents('grunbergsalt.csv');
+		file_put_contents('grunbergsalt.csv', $fileContent);
+	}
 }
 ?>
