ccbf10dbe77ee640f511fa5c3a9886760d312593
Phihili <you@example.com>
2018-05-21 15:59:33 +0200

    Hotfix

--- addSalt.php
+++ addSalt.php
@@ -1,9 +1,15 @@
 <?php 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	if($_POST['text'] == "proclaim") {
-		echo "En slackbot selvfølgelig! Skriv /salt efterfulgt af Grünbergs salt og det ender direkte på https://GrunbergSalt.life!";
+		echo "  {
+				    \"response_type\": \"in_channel\",
+				    \"text\": \"En slackbot selvfølgelig! Skriv /salt efterfulgt af Grünbergs salt og det ender direkte på https://GrunbergSalt.life!\",
+				}";
 	} else {
-		echo "Added \"" . $_POST['text'] . "\" to https://GrunbergSalt.life!";
+		echo "  {
+				    \"response_type\": \"in_channel\",
+				    \"text\": \"Added \"" . $_POST['text'] . "\" to https://GrunbergSalt.life!\",
+				}";
 		$fileContent = $_POST['text'] . "\n" . file_get_contents('grunbergsalt.csv');
 		file_put_contents('grunbergsalt.csv', $fileContent);
 	}
