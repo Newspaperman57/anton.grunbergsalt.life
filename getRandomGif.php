8b7bfa7449a61a209dd9faf8c377ae9119daaef0
Phihili <you@example.com>
2018-05-23 12:30:17 +0200

    Changes random gifs

--- getRandomGif.php
+++ getRandomGif.php
@@ -1,6 +1,6 @@
 <?php 
 	function getRandomGifURL() {
-		$gifSearchContent = file_get_contents("http://api.giphy.com/v1/gifs/search?api_key=HIwHCwVBiZXYBFLu56t4IuhYa8rnbHfK&q=dont&limit=1&offset=". rand(1, 500));
+		$gifSearchContent = file_get_contents("http://api.giphy.com/v1/gifs/search?api_key=HIwHCwVBiZXYBFLu56t4IuhYa8rnbHfK&q=fuck-you&limit=1&offset=". rand(1, 100));
 		$gifContentJSON = json_decode($gifSearchContent, true);
 		return 'http://i.' . substr($gifContentJSON['data'][0]['images']['fixed_height']['url'], 15);
 	}
